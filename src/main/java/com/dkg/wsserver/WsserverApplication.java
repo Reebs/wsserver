ef1d5cd272c6b65528597eaef15ae38554fdc1b6
jrebe <jrebe@allstate.com>
2018-02-13 16:12:05 -0600

    initial check in

---
+++ src/main/java/com/dkg/wsserver/WsserverApplication.java
@@ -0,0 +1,12 @@
+package com.dkg.wsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class WsserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsserverApplication.class, args);
+	}
+}
